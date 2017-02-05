fc4a7b74d373d06cdf865f699173362b5a86857f
Ryan Thompson <ryan@pyrocms.com>
2017-02-05 12:24:26 -0600

    Fix weird ass issue where plugin was parsing the shittiest data you've ever seen into the value.

--- resources/js/input.js
+++ resources/js/input.js
@@ -10,7 +10,7 @@ $(document).on('ajaxComplete ready', function () {
         var modal = $('#' + field + '-modal');
         var wrapper = input.closest('.form-group');
         var image = wrapper.find('[data-provides="cropper"]');
-        
+
         var options = {
             viewMode: 0,
             zoomable: false,
@@ -18,12 +18,21 @@ $(document).on('ajaxComplete ready', function () {
             data: image.data('data'),
             aspectRatio: image.data('aspect-ratio'),
             minContainerHeight: image.data('min-container-height'),
-            build: function() {
+            build: function () {
                 if (image.attr('src').length) {
                     image.closest('.cropper').removeClass('hidden');
                 }
             },
-            crop: function(e) {
+            crop: function (e) {
+
+                /**
+                 * This prevents trashy data from
+                 * being parsed into the field value.
+                 */
+                if (!isFinite(e.x) || isNaN(e.x) || typeof e.x == 'undefined' || e.x == null || (e.x == 0 && e.y == 0)) {
+                    return;
+                }
+
                 $('[name="' + field + '[data]"]').val(JSON.stringify({
                     'x': e.x,
                     'y': e.y,
@@ -46,13 +55,13 @@ $(document).on('ajaxComplete ready', function () {
 
         image.cropper(options);
 
-        modal.on('click', '[data-file]', function(e) {
+        modal.on('click', '[data-file]', function (e) {
 
             e.preventDefault();
 
             modal.find('.modal-content').append('<div class="modal-loading"><div class="active loader"></div></div>');
 
-            wrapper.find('.selected').load('/streams/image-field_type/selected?uploaded=' + $(this).data('file'), function() {
+            wrapper.find('.selected').load('/streams/image-field_type/selected?uploaded=' + $(this).data('file'), function () {
 
                 modal.modal('hide');
             });
@@ -66,14 +75,14 @@ $(document).on('ajaxComplete ready', function () {
             $('[name="' + field + '[id]"]').val($(this).data('file'));
         });
 
-        $(wrapper).on('click', '[data-dismiss="file"]', function(e) {
+        $(wrapper).on('click', '[data-dismiss="file"]', function (e) {
 
             e.preventDefault();
 
             $('[name="' + field + '[id]"]').val('');
             $('[name="' + field + '[data]"]').val('');
 
-            wrapper.find('.selected').load('/streams/image-field_type/selected', function() {
+            wrapper.find('.selected').load('/streams/image-field_type/selected', function () {
 
                 modal.modal('hide');
 
