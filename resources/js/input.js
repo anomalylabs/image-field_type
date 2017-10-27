b82639fd3dbbb38f2fd97667a6d1790e0d59ec1a
Ryan Thompson <ryan@pyrocms.com>
2017-10-27 09:48:27 -0500

    Fixes n improvements

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,16 +1,16 @@
-$(document).on('ajaxComplete ready', function () {
+(function (window, document) {
 
     // Initialize file pickers
     $('[data-provides="anomaly.field_type.image"]:not([data-initialized])').each(function () {
 
         $(this).attr('data-initialized', '');
 
-        let input = $(this);
-        let field = input.data('field_name');
-        let modal = $('#' + field + '-modal');
-        let wrapper = input.closest('.form-group');
-        let image = wrapper.find('[data-provides="cropper"]');
-        let toggle = wrapper.find('[data-toggle="cropper"]');
+        let $input = $(this);
+
+        let $wrapper = $input.closest('.form-group');
+        let $image = $wrapper.find('[data-provides="cropper"]');
+        let $toggle = $wrapper.find('[data-toggle="cropper"]');
+        let $modal = $('#' + $input.data('field_name') + '-modal');
 
         let options = {
             viewMode: 0,
@@ -19,9 +19,8 @@ $(document).on('ajaxComplete ready', function () {
             autoCropArea: 1,
             responsive: true,
             checkOrientation: false,
-            data: image.data('data'),
-            aspectRatio: image.data('aspect-ratio'),
-            minContainerHeight: image.data('min-container-height'),
+            data: $image.data('data'),
+            aspectRatio: $image.data('aspect-ratio'),
             crop: function (e) {
 
                 /**
@@ -32,7 +31,7 @@ $(document).on('ajaxComplete ready', function () {
                     return;
                 }
 
-                $('[name="' + field + '[data]"]').val(JSON.stringify({
+                $('[name="' + $input.data('field_name') + '[data]"]').val(JSON.stringify({
                     'x': e.x,
                     'y': e.y,
                     'width': e.width,
@@ -44,47 +43,54 @@ $(document).on('ajaxComplete ready', function () {
             }
         };
 
-        toggle.on('click', function () {
+        $toggle.on('click', function () {
 
-            image.closest('.cropper').removeClass('hidden');
-            image.cropper(options);
+            $image
+                .closest('.cropper')
+                .removeClass('hidden');
+
+            $image.cropper(options);
 
             return false;
         });
 
-        modal.on('click', '[data-file]', function (e) {
+        $modal.on('click', '[data-file]', function (e) {
 
             e.preventDefault();
 
-            modal.find('.modal-content').append('<div class="modal-loading"><div class="active loader"></div></div>');
+            let $button = $(this);
 
-            wrapper.find('.selected').load('/streams/image-field_type/selected?uploaded=' + $(this).data('file'), function () {
-                modal.modal('hide');
+            $modal.find('.modal-content').append('<div class="modal-loading"><div class="active loader"></div></div>');
+
+            $wrapper.find('.selected').load('/streams/image-field_type/selected?uploaded=' + $button.data('file'), function () {
+                $modal.modal('hide');
             });
 
-            image.closest('.cropper').removeClass('hidden');
+            $image.closest('.cropper').removeClass('hidden');
 
-            image
+            $image
                 .cropper(options)
-                .cropper('replace', '/streams/image-field_type/view/' + $(this).data('file'))
+                .cropper('replace', '/streams/image-field_type/view/' + $button.data('file'))
                 .cropper('reset');
 
-            $('[name="' + field + '[id]"]').val($(this).data('file'));
+            $('[name="' + $input.data('field_name') + '[id]"]').val($button.data('file'));
         });
 
-        $(wrapper).on('click', '[data-dismiss="file"]', function (e) {
+        $wrapper.on('click', '[data-dismiss="file"]', function (e) {
 
             e.preventDefault();
 
-            $('[name="' + field + '[id]"]').val('');
-            $('[name="' + field + '[data]"]').val('');
+            $('[name="' + $input.data('field_name') + '[id]"]').val('');
+            $('[name="' + $input.data('field_name') + '[data]"]').val('');
 
-            wrapper.find('.selected').load('/streams/image-field_type/selected', function () {
+            $wrapper.find('.selected').load('/streams/image-field_type/selected', function () {
 
-                modal.modal('hide');
+                $modal.modal('hide');
 
-                image.closest('.cropper').addClass('hidden');
+                $image
+                    .closest('.cropper')
+                    .addClass('hidden');
             });
         });
     });
-});
+})(window, document);
